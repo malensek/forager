743f24e47628e2adb43dfdcb93dbe2db85f08beb
Matthew Malensek <matt@malensek.net>
2014-04-27 22:12:43 -0600

    Add event map base class

---
+++ src/forager/server/EventMap.java
@@ -0,0 +1,26 @@
+
+package forager.server;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EventMap {
+
+    protected static final Map<Integer, Class<?>> intToClass = new HashMap<>();
+    protected static final Map<Class<?>, Integer> classToInt = new HashMap<>();
+
+    public EventMap() { }
+
+    protected static void addMapping(int id, Class<?> clazz) {
+        intToClass.put(id, clazz);
+        classToInt.put(clazz, id);
+    }
+
+    public static Class<?> getClass(int id) {
+        return intToClass.get(id);
+    }
+
+    public static int getInt(Class<?> clazz) {
+        return classToInt.get(clazz);
+    }
+}
