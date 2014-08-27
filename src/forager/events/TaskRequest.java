6f90e055940f497dbf0c0eba5de57124b8ef8a13
Matthew Malensek <matt@malensek.net>
2014-08-27 11:04:17 -0600

    Add a numTasks parameter to TaskRequest

--- src/forager/events/TaskRequest.java
+++ src/forager/events/TaskRequest.java
@@ -1,5 +1,28 @@
 package forager.events;
 
-import galileo.event.StubEvent;
+import java.io.IOException;
 
-public class TaskRequest extends StubEvent { }
+import galileo.event.Event;
+import galileo.serialization.SerializationInputStream;
+import galileo.serialization.SerializationOutputStream;
+
+public class TaskRequest implements Event {
+
+    public int numTasks;
+
+    public TaskRequest(int numTasks) {
+        this.numTasks = numTasks;
+    }
+
+    @Deserialize
+    public TaskRequest(SerializationInputStream in)
+    throws IOException {
+        this.numTasks = in.readInt();
+    }
+
+    @Override
+    public void serialize(SerializationOutputStream out)
+    throws IOException {
+        out.writeInt(numTasks);
+    }
+}
