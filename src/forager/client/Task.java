e0a4c1108df61ceb12efdb8af821b729c4bcd4fd
Matthew Malensek <matt@malensek.net>
2014-08-27 11:04:46 -0600

    Redirect output to stdout/err for tasks

--- src/forager/client/Task.java
+++ src/forager/client/Task.java
@@ -1,5 +1,7 @@
 package forager.client;
 
+import java.lang.ProcessBuilder.Redirect;
+
 public class Task implements Runnable {
 
     private String[] command;
@@ -11,6 +13,10 @@ public class Task implements Runnable {
     public void run() {
         try {
             ProcessBuilder pb = new ProcessBuilder(command);
+
+            pb.redirectOutput(Redirect.INHERIT);
+            pb.redirectError(Redirect.INHERIT);
+
             Process p = pb.start();
             p.waitFor();
         } catch (Exception e) {
