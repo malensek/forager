30b76ac69ec81adccb12e83538c49d6a5f0dbd97
Matthew Malensek <matt@malensek.net>
2014-09-01 23:28:05 -0600

    Remove JoinEvent from EventMap

--- src/forager/events/ForagerEventMap.java
+++ src/forager/events/ForagerEventMap.java
@@ -30,7 +30,6 @@ import galileo.event.EventMap;
 public class ForagerEventMap extends EventMap {
 
     public ForagerEventMap() {
-        addMapping(JoinEvent.class);
         addMapping(TaskRequest.class);
         addMapping(TaskSpec.class);
     }
