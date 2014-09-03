86a8a8b72ee4700a3b7359628241c934942442f2
Matthew Malensek <matt@malensek.net>
2014-09-03 10:19:04 -0600

    Move default port to UI

--- src/forager/server/Overlord.java
+++ src/forager/server/Overlord.java
@@ -46,8 +46,6 @@ import galileo.net.ServerMessageRouter;
 
 public class Overlord {
 
-    public static final int DEFAULT_PORT = 53380;
-
     private static final Logger logger = Logger.getLogger("forager");
 
     private int port;
@@ -58,10 +56,6 @@ public class Overlord {
 
     private int taskCounter = 0;
     private Queue<String> taskList = new LinkedList<>();
-
-    public Overlord() {
-        this.port = DEFAULT_PORT;
-    }
 
     public Overlord(int port) {
         this.port = port;
