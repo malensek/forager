5c7f1c95bad9829d15cae238d617a4ddededae50
Matthew Malensek <matt@malensek.net>
2014-09-04 15:37:30 -0600

    Switch to single-string commands and long ids

--- src/forager/events/TaskSpec.java
+++ src/forager/events/TaskSpec.java
@@ -1,18 +1,22 @@
 package forager.events;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import galileo.event.Event;
+import galileo.net.NetworkDestination;
 import galileo.serialization.SerializationInputStream;
 import galileo.serialization.SerializationOutputStream;
 
 public class TaskSpec implements Event {
 
-    public int taskId;
-    public String[] command;
+    public long taskId;
+    public String command;
 
-    public TaskSpec(int taskId, String[] command) {
     private List<NetworkDestination> assignments = new ArrayList<>();
+
+    public TaskSpec(long taskId, String command) {
         this.taskId = taskId;
         this.command = command;
     }
@@ -36,31 +40,20 @@ public class TaskSpec implements Event {
     }
 
     public String toString() {
-        String str = "[" + taskId + "] ";
-        for (String s : command) {
-            str += s + " ";
-        }
-        return str;
+        return "[" + taskId + "] " + command;
     }
 
     @Deserialize
     public TaskSpec(SerializationInputStream in)
     throws IOException {
-        taskId = in.readInt();
-        int numArgs = in.readInt();
-        command = new String[numArgs];
-        for (int i = 0; i < numArgs; ++i) {
-            command[i] = in.readString();
-        }
+        taskId = in.readLong();
+        command = in.readString();
     }
 
     @Override
     public void serialize(SerializationOutputStream out)
     throws IOException {
-        out.writeInt(taskId);
-        out.writeInt(command.length);
-        for (String s : command) {
-            out.writeString(s);
-        }
+        out.writeLong(taskId);
+        out.writeString(command);
     }
 }
