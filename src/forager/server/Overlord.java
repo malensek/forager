99b827ac8bc1e7a6f14a304fc2fb2e99241785b0
Matthew Malensek <matt@malensek.net>
2014-09-07 23:42:27 -0600

    Remove addTaskFile() method

--- src/forager/server/Overlord.java
+++ src/forager/server/Overlord.java
@@ -109,19 +109,6 @@ public class Overlord {
         }
     }
 
-//    public void addTaskFile(String file)
-//    throws FileNotFoundException, IOException {
-//        BufferedReader br = new BufferedReader(new FileReader(file));
-//        int added = 0;
-//        String line;
-//        while ((line = br.readLine()) != null) {
-//            taskList.add(line);
-//            added++;
-//        }
-//        br.close();
-//        logger.log(Level.INFO, "Added {0} tasks.", added);
-//    }
-
     @EventHandler
     public void processTaskRequest(TaskRequest request, EventContext context) {
 
