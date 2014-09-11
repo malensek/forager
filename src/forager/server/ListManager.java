fc8025d224057a7269876ce13d63e20a6402aa2c
Matthew Malensek <matt@malensek.net>
2014-09-10 23:51:12 -0600

    Add completed task file implementation

--- src/forager/server/ListManager.java
+++ src/forager/server/ListManager.java
@@ -8,17 +8,35 @@ import java.io.PrintWriter;
 
 public class ListManager {
 
+    public static final String DEFAULT_LIST_NAME = "tasklist";
+
     private FileOutputStream taskListOut;
     private PrintWriter taskListWriter;
 
+    private FileOutputStream completedListOut;
+    private PrintWriter completedListWriter;
+
     public ListManager() throws IOException {
-        String taskList = "tasklist";
-        taskListOut = new FileOutputStream(taskList, true);
-        taskListWriter = new PrintWriter(new BufferedOutputStream(taskListOut));
+        this(DEFAULT_LIST_NAME);
+    }
+
+    public ListManager(String taskListName) throws IOException {
+        taskListOut = new FileOutputStream(taskListName, true);
+        taskListWriter = new PrintWriter(
+                new BufferedOutputStream(taskListOut));
+
+        String completedName = taskListName + ".done";
+        completedListOut = new FileOutputStream(completedName, true);
+        completedListWriter = new PrintWriter(
+                new BufferedOutputStream(completedListOut));
     }
 
     public void addTask(String command) {
         taskListWriter.println(command);
+    }
+
+    public void addCompletedTask(String command) {
+        completedListWriter.println(command);
     }
 
     public void sync() throws IOException {
