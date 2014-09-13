14dc406ad95767c1a2bad3a06234fb43b7ac1c99
Matthew Malensek <matt@malensek.net>
2014-09-12 22:51:39 -0600

    Add task list parameter and default

--- src/forager/ui/Server.java
+++ src/forager/ui/Server.java
@@ -10,12 +10,18 @@ import forager.server.Overlord;
 public class Server implements CommandLauncher {
 
     public static final int DEFAULT_PORT = 53380;
+    public static final String DEFAULT_TASKLIST = "tasklist";
+
     @Parameter(names = { "-p", "--port" }, description = "Server port")
     private int port = DEFAULT_PORT;
 
+    @Parameter(names = { "-t", "--task-list" },
+            description = "Task list location")
+    private String taskList = DEFAULT_TASKLIST;
+
     @Override
     public void launch() throws Exception {
-        Overlord server = new Overlord(port);
+        Overlord server = new Overlord(port, taskList);
         server.start();
     }
 }
