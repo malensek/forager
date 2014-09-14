ced9b7c0345113afd9971af4cd1a7d10f384cb9c
Matthew Malensek <matt@malensek.net>
2014-09-14 00:36:48 -0600

    Add exit code field/serialization

--- src/forager/events/TaskCompletion.java
+++ src/forager/events/TaskCompletion.java
@@ -9,20 +9,29 @@ import java.io.IOException;
 public class TaskCompletion implements Event {
 
     public long taskId;
+    public int exitCode;
 
     public TaskCompletion(long taskId) {
         this.taskId = taskId;
+        this.exitCode = 0;
+    }
+
+    public TaskCompletion(long taskId, int exitCode) {
+        this.taskId = taskId;
+        this.exitCode = exitCode;
     }
 
     @Deserialize
     public TaskCompletion(SerializationInputStream in)
     throws IOException {
         taskId = in.readLong();
+        exitCode = in.readInt();
     }
 
     @Override
     public void serialize(SerializationOutputStream out)
     throws IOException {
         out.writeLong(taskId);
+        out.writeInt(exitCode);
     }
 }
