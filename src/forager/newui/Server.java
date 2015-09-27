1c27a22a9ffe02de4ed36bbb523a90f02d7c3940
Matthew Malensek <matt@malensek.net>
2015-09-26 23:53:37 -0600

    Create server command

---
+++ src/forager/newui/Server.java
@@ -0,0 +1,11 @@
+package forager.newui;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+import joptsimple.OptionParser;
+import joptsimple.OptionSpec;
+
+public class Server implements Command {
+}
