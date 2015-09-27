3aecc75f455b9df97f69c5c5579fb6ea3a98393d
Matthew Malensek <matt@malensek.net>
2015-09-26 23:54:22 -0600

    Add execute method
    Port, reset, and task list options

--- src/forager/newui/Server.java
+++ src/forager/newui/Server.java
@@ -11,4 +11,25 @@ public class Server implements Command {
 
     public static int DEFAULT_PORT = 53380;
 
+    private OptionParser parser = new OptionParser();
+
+    public void execute(String[] args) throws Exception {
+        OptionSpec<Integer> port = parser.acceptsAll(
+                Arrays.asList("p", "port"), "Server port")
+            .withRequiredArg()
+            .ofType(Integer.class)
+            .defaultsTo(DEFAULT_PORT);
+
+        OptionSpec<?> reset = parser.acceptsAll(
+                Arrays.asList("r", "reset"), "Reset (clear) the task list");
+
+        OptionSpec<File> taskList = parser.acceptsAll(
+                Arrays.asList("t", "tasklist", "task-list"),
+                "Path to the task list (created if it doesn't exist)")
+            .withRequiredArg()
+            .ofType(File.class)
+            .defaultsTo(new File("./tasklist"));
+
+        printUsage();
+    }
 }
